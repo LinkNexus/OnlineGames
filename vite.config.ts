271c380d67201c9b7de860ecd19e70cbfc8d4c03
LinkNexus <deitykings@gmail.com>
2024-12-21 13:03:56 +0100

    Installed Vite, Typescript and React (through Stimulus Bundle)

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import symfonyPlugin from "vite-plugin-symfony";
+import path from "path"
+
+/* if you're using React */
+import reactPlugin from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        reactPlugin(), // if you're using React
+        symfonyPlugin({
+            stimulus: true
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                app: "./assets/app.js"
+            },
+        }
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./assets"),
+        },
+    },
+});
