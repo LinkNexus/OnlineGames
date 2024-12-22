6e60ba43901622249810c5000017f2bd3e95a06e
LinkNexus <deitykings@gmail.com>
2024-12-22 01:11:55 +0100

    Set Up Mercure, started testing it.

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AppController extends AbstractController
@@ -11,8 +13,19 @@ class AppController extends AbstractController
     #[Route('/app', name: 'app_app')]
     public function index(): Response
     {
-        return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
-        ]);
+        return $this->render('app/index.html.twig');
+    }
+
+    #[Route('/publish', name: 'app_publish', methods: ['POST'])]
+    public function publish(HubInterface $hub): Response
+    {
+        $hub->publish(
+            new Update(
+                'http://localhost:5001/test',
+                json_encode(['status' => 'Mercure is functioning!'])
+            )
+        );
+
+        return $this->redirectToRoute('app_app');
     }
 }
